59cd8357fe52fe5dacd3ae0abbe396b782c7afd4
tukki0210 <tukki0210@gmail.com>
2024-07-09 13:37:59 +0900

    奇数偶数の判定

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const A = document.querySelector('#A')
 
+let count = 0
+
 A.addEventListener('click', () => {
-    A.textContent = '〇'
+    count++;
+    // 奇数偶数の判定
+    if (count%2===1){
+        A.textContent = '〇'
+    } else {
+        A.textContent = '×'
+    }
 })
