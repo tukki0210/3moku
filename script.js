e56a8e9ede204dfeb0d545881eb1f8dba8935b7e
tukki0210 <tukki0210@gmail.com>
2024-07-09 15:21:36 +0900

    〇×判定と関数

--- script.js
+++ script.js
@@ -40,6 +40,14 @@ console.log(masu);
 
 let count = 0
 
+// 判定するための関数
+const judge = (a, b, c) => {
+    return masu[a].textContent === '〇' &&
+        masu[b].textContent === '〇' &&
+        masu[c].textContent === '〇'
+}
+
+
 for (let i = 0; i < 9; i++) {
     masu[i].addEventListener('click', () => {
         count++;
@@ -53,15 +61,48 @@ for (let i = 0; i < 9; i++) {
             masu[i].classList.add('batu');
         }
 
-        if (masu[0].textContent === '〇' &&
+        // 勝敗の判定
+        if (judge(0, 1, 2)) {
+            console.log('〇の勝ち！')
+        } else if (judge(3, 4, 5)) {
+            console.log('〇の勝ち！')
+        }
+        else if (judge(6, 7, 8)) {
+            console.log('〇の勝ち！')
+        } else if (
+            masu[0].textContent === '〇' &&
+            masu[3].textContent === '〇' &&
+            masu[6].textContent === '〇'
+        ) {
+            console.log('〇の勝ち！')
+        } else if (
             masu[1].textContent === '〇' &&
-            masu[2].textContent === '〇'
-        ){
-            console.log(masu[0].textContent)
+            masu[4].textContent === '〇' &&
+            masu[7].textContent === '〇'
+        ) {
+            console.log('〇の勝ち！')
+        } else if (
+            masu[2].textContent === '〇' &&
+            masu[5].textContent === '〇' &&
+            masu[8].textContent === '〇'
+        ) {
+            console.log('〇の勝ち！')
+        } else if (
+            masu[0].textContent === '〇' &&
+            masu[4].textContent === '〇' &&
+            masu[8].textContent === '〇'
+        ) {
+            console.log('〇の勝ち！')
+        } else if (
+            masu[2].textContent === '〇' &&
+            masu[4].textContent === '〇' &&
+            masu[6].textContent === '〇'
+        ) {
             console.log('〇の勝ち！')
         }
     })
 }
+
 
 // イベントの外は読み込んだ瞬間に実行される
 // if (masu[0].textContent === '〇'){
