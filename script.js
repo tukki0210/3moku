3374809afd01ea09c85a13e6a896df3ad2025aad
tukki0210 <tukki0210@gmail.com>
2024-07-09 14:54:02 +0900

    横1行そろったとき

--- script.js
+++ script.js
@@ -43,12 +43,28 @@ let count = 0
 for (let i = 0; i < 9; i++) {
     masu[i].addEventListener('click', () => {
         count++;
+
         // 奇数偶数の判定
         if (count % 2 === 1) {
             masu[i].textContent = '〇'
+            masu[i].classList.add('maru');
         } else {
             masu[i].textContent = '×'
+            masu[i].classList.add('batu');
+        }
+
+        if (masu[0].textContent === '〇' &&
+            masu[1].textContent === '〇' &&
+            masu[2].textContent === '〇'
+        ){
+            console.log(masu[0].textContent)
+            console.log('〇の勝ち！')
         }
     })
 }
 
+// イベントの外は読み込んだ瞬間に実行される
+// if (masu[0].textContent === '〇'){
+//     console.log(masu[0].textContent)
+//     console.log('〇の勝ち！')
+// }
