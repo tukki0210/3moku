a977fdea89f2270bc02fed1d48815161f6c91da6
tukki0210 <tukki0210@gmail.com>
2024-07-09 14:15:19 +0900

    querySelectorAllを使う

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
-const A = document.querySelector('#A')
+// const A = document.querySelector('#A')
+// const B = document.querySelector('#B')
+// const C = document.querySelector('#C')
+
+// let count = 0
+
+// A.addEventListener('click', () => {
+//     count++;
+//     // 奇数偶数の判定
+//     if (count%2===1){
+//         A.textContent = '〇'
+//     } else {
+//         A.textContent = '×'
+//     }
+// })
+
+// B.addEventListener('click', () => {
+//     count++;
+//     // 奇数偶数の判定
+//     if (count%2===1){
+//         B.textContent = '〇'
+//     } else {
+//         B.textContent = '×'
+//     }
+// })
+
+// C.addEventListener('click', () => {
+//     count++;
+//     // 奇数偶数の判定
+//     if (count%2===1){
+//         C.textContent = '〇'
+//     } else {
+//         C.textContent = '×'
+//     }
+// })
+
+// これは配列が入る
+const masu = document.querySelectorAll('.board div');
+console.log(masu);
 
 let count = 0
 
-A.addEventListener('click', () => {
+masu[0].addEventListener('click', () => {
     count++;
     // 奇数偶数の判定
-    if (count%2===1){
-        A.textContent = '〇'
+    if (count % 2 === 1) {
+        masu[0].textContent = '〇'
     } else {
-        A.textContent = '×'
+        masu[0].textContent = '×'
     }
 })
